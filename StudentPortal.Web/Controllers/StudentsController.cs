b4187d94e09caa1d44b91f816400a2b8dd758b3b
Nimisha <nimishawadkar1902@gmail.com>
2024-10-05 20:27:50 +0530

    Add project files.

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models.Entities;
+using StudentPortal.Web.Models.ViewModels;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public StudentsController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ActionName("Add")]
+        public async Task<IActionResult> Create(AddStudentViewModel addStudentViewModel)
+        {
+            var student = new Student
+            {
+                Name = addStudentViewModel.Name,
+                Email = addStudentViewModel.Email,
+                Phone = addStudentViewModel.Phone,
+                Subscribed = addStudentViewModel.Subscribed,
+                Dob = addStudentViewModel.Dob,
+            };
+            await applicationDbContext.Students.AddAsync(student);
+            await applicationDbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await applicationDbContext.Students.ToListAsync();
+            return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await applicationDbContext.Students.FindAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student studentlist)
+        {
+           var student = await applicationDbContext.Students.FindAsync(studentlist.Id);
+            if(student != null)
+            {
+                student.Name = studentlist.Name;
+                student.Email = studentlist.Email;
+                student.Phone = studentlist.Phone;
+                student.Subscribed = studentlist.Subscribed;
+                student.Dob = studentlist.Dob;
+
+                await applicationDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student delstudent)
+        {
+            var student = await applicationDbContext.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == delstudent.Id);
+
+            if (student != null)
+            {
+                applicationDbContext.Students.Remove(delstudent);
+                await applicationDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Students");
+        }
+
+
+    }
+}
---
+++ StudentPortal.Web/Migrations/20241004153031_nn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentPortal.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class nn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DOB",
+                table: "Students",
+                newName: "Dob");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Dob",
+                table: "Students",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Dob",
+                table: "Students",
+                newName: "DOB");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DOB",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
